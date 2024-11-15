b12da0d2b5563978e99667552c5acf3fc78ae7a9
Yevhen Chementsov <yevhenchementsov@gmail.com>
2024-11-15 13:23:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,64 +9,63 @@ let lastETag = null;
 let lastModified = null;
 let lastContentHash = null;
 
+const prepareHeaders = () => {
+  const headers = { 'User-Agent': 'Mozilla/5.0 (compatible; MyScript/1.0)' };
+  if (lastETag) headers['If-None-Match'] = lastETag;
+  if (lastModified) headers['If-Modified-Since'] = lastModified;
+  return headers;
+};
+
 const checkForUpdates = async () => {
   try {
+    const response = await fetch(URL, { headers: prepareHeaders() });
+
+    if (response.status === 304) {
+      logUpdateMessage('No update found (304 Not Modified).');
+      return;
+    }
+
+    if (response.status !== 200) {
+      logUpdateMessage(`Error: Received status ${response.status}`);
+      return;
+    }
+
+    const data = await response.json();
+    const dataToString = JSON.stringify(data, null, 2);
     const headers = {
-      'User-Agent': 'Mozilla/5.0 (compatible; MyScript/1.0)',
+      etag: response.headers.get('ETag'),
+      modified: response.headers.get('Last-Modified'),
+    };
+    const contentHash = getHash(dataToString);
+    const changes = {
+      etagChanged: headers.etag !== lastETag,
+      modifiedChanged: headers.modified !== lastModified,
+      contentChanged: contentHash !== lastContentHash,
     };
 
-    if (lastETag) headers['If-None-Match'] = lastETag;
-    if (lastModified) headers['If-Modified-Since'] = lastModified;
-
-    const response = await fetch(URL, { headers });
-
-    switch (response.status) {
-      case 304:
-        logUpdateMessage('No update found (304 Not Modified).');
-        return;
-      case 200:
-        const data = await response.json();
-        const dataToString = JSON.stringify(data, null, 2);
-        const headers = {
-          etag: response.headers.get('ETag'),
-          modified: response.headers.get('Last-Modified'),
-        };
-        const contentHash = getHash(dataToString);
-        const changes = {
-          etagChanged: headers.etag !== lastETag,
-          modifiedChanged: headers.modified !== lastModified,
-          contentChanged: contentHash !== lastContentHash,
-        };
-
-        if (changes.etagChanged) {
-          lastETag = headers.etag;
-          logUpdateMessage('Change detected: ETag has changed.');
-        } else if (changes.modifiedChanged) {
-          lastModified = headers.modified;
-          logUpdateMessage(
-            'Change detected: Last-Modified header has changed.',
-          );
-        } else if (changes.contentChanged) {
-          lastContentHash = contentHash;
-          logUpdateMessage('Change detected: Content hash has changed.');
-        } else {
-          logUpdateMessage('No change detected.');
-          return;
-        }
-
-        await fs.writeFile(dbFilePath, dataToString);
-        logUpdateMessage('Update found and saved.');
-        break;
-      default:
-        const errorMessage = `Error: Received status ${response.status}`;
-        logUpdateMessage(errorMessage);
+    if (changes.etagChanged) {
+      lastETag = headers.etag;
+      logUpdateMessage('Change detected: ETag has changed.');
+    } else if (changes.modifiedChanged) {
+      lastModified = headers.modified;
+      logUpdateMessage('Change detected: Last-Modified header has changed.');
+    } else if (changes.contentChanged) {
+      lastContentHash = contentHash;
+      logUpdateMessage('Change detected: Content hash has changed.');
+    } else {
+      logUpdateMessage('No change detected.');
+      return;
     }
+
+    await fs.writeFile(dbFilePath, dataToString);
+    logUpdateMessage('Update found and saved.');
   } catch (error) {
-    const errorMessage = `Error fetching file: ${error.message}`;
-    logUpdateMessage(errorMessage);
+    logUpdateMessage(`Error during update check: ${error.message}`);
   }
 };
 
-await checkForUpdates(); //! commit this line if you run it locally
+//! comment the line below if running locally
+await checkForUpdates();
+
 // Run every hour
-// setInterval(checkForUpdates, 60 * 1000); //! commit this line if you run it remotely
+// setInterval(checkForUpdates, 60 * 60 * 1000); //! comment this line for remote execution
